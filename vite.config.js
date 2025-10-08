2d7045e3a5006b1aa76c77ccb602dacd5d5d5bd1
skmdashan@gmail.com <ashanshaik987@gmail.com>
2025-10-08 19:25:44 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-website2/',
-  plugins: [tailwindcss(),react()],
+  base: '/', 
+  plugins: [tailwindcss(), react()],
 })
